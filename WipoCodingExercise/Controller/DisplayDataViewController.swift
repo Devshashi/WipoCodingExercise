95c4f668a014333b88f2bcb0f5a5eae67072597b
devshashi <devshashi01@gmail.com>
2020-07-03 01:33:34 +0530

    Complete project setup

---
+++ WipoCodingExercise/Controller/DisplayDataViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayDataViewController.swift
+//  WipoCodingExercise
+//
+//  Created by Devshashi on 03/07/20.
+//  Copyright © 2020 Devshashi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class DisplayDataViewController : UIViewController {
+    
+    
+    
+    
+    
+}
